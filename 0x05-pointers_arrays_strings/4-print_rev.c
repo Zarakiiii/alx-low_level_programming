89a0ce4e5eeb612597ec1041522b3e2dedf2c58c
amine <amineaslim@gmail.com>
2023-06-25 23:56:50 -0700

    fix7

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
-  /** 
-   * print_rev- print reversed string, followed by a new line
-   * @s: pointer to the string to point 
-   * Return: void
-   */
-
-   void _print_rev(char *s) 
-  {
-     int swap;
-
-     int i = 0;
-     while (s[i]) 
-            i++;
-  while (i--) 
-{  
-      _putchar(s[i]);
-  } 
-     _putchar('/n');
-
-  } 
+/**
+* print_rev - Prints a reversed string, followed by a new line.
+*
+* @s: Pointer to the string to print.
+*
+* Return: void
+*/
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
