3538f4ebd1938e86a272ad88d0b87de4e369ad34
amine <amineaslim@gmail.com>
2023-06-25 22:32:20 -0700

    reset0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
-// Author By Eng Ismail
-// URL = https://github.com/macaanto14
 /**
- 
- * @n: An integer input pointer
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
+
 void reset_to_98(int *n)
 {
 	*n = 98;
