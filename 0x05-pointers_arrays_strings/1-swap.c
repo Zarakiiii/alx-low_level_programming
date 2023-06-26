c7959c24d49a84850dfcdb545338cc6bb18faec7
amine <amineaslim@gmail.com>
2023-06-25 22:49:19 -0700

    fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,13 @@
- include "main.h"
-
-  /** 
-   * swap_int - swapping variables 
-   * @a: pointer 1
-   * @b: pointer 2
-   */
-   void swap_int(int *a, int *b) 
-  {
-     int swap;
-
-   swap = *a;
-   *a = *b;
-    *b = swap;
-
-  } 
+include "main.h"
+/**
+* swap_int-swapping variables
+* @a:pointer1
+* @b:pointer2
+*/
+void swap_int(int *a, int *b)
+{
+int swap;
+swap = *a;
+*a = *b;
+*b = swap;
+}
