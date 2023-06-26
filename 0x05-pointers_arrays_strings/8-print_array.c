faa7500c1fdf2cc564bad84e3660d451a8c57586
amine <amineaslim@gmail.com>
2023-06-26 00:19:17 -0700

    fix11

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,30 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * puts_half - Prints half of a string followed by a new line.
- *
- * @str: String to be printed.
- */
-void puts_half(char *str)
+* print_array - prints n elements of an array of integers
+* followed by a new line
+* @a: array to be printed
+* @n: number of elements to print
+*/
+void print_array(int *a, int n)
 {
-int len, n, i;
-len = 0;
-while (str[len] != '\0')
+int i;
+for (i = 0; i < n; i++)
 {
-len++;
-}
-if (len % 2 == 0)
-{
-for (i = len / 2; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-}
+if (i == 0)
+printf("%d", a[i]);
 else
-{
-for (n = (len + 1) / 2; n < len; n++)
-{
-_putchar(str[n]);
+printf(", %d", a[i]);
 }
-}
-_putchar('\n');
+printf("\n");
 }
